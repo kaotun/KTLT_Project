1f85ab19fc0741db7ae1927f109b871a48c6a58d
kaotun <tuancaoquoc2005@gmail.com>
2024-05-18 22:17:12 +0700

    init project

---
+++ KTLTProject/AddStudent.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ChangeInfo.h"
+#include "ConsoleProcess.h"
+#include "CourseFunction.h"
+#include "Struct.h"
+#include "ListClass.h"
+#include "Login.h"
+#include "Menu.h"
+
+void AddStudentToClass();
+void AddRandomInfoStudentToClass();
+int RunMenuAddInfoStudentToClass();
