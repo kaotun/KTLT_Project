6241d57ec5c506a74e018afcae115526eaaee3a9
lehoangviet <139225046+github-0507@users.noreply.github.com>
2024-06-20 20:54:38 +0700

    Update StaffFunction.cpp
    Thêm hàm AddSemeter và hàm CreateRegistrationCourse

--- KTLTProject/StaffFunction.cpp
+++ KTLTProject/StaffFunction.cpp
@@ -304,3 +304,235 @@ void UpDateFileInfo(Semester Semestertmp){
     file << endl << "End Date: " << Semestertmp.DateEnd;
     file.close();
 }
+
+void AddSemester(string& YearNow, string& SemesterNow) {
+    char ch;
+    Semester semester;
+    system("cls");
+    TextBgColor(4, 7);
+    PrintText("  ____                           _            ", 40, 2);
+    PrintText(" / ___|  ___ _ __ ___   ___  ___| |_ ___ _ __ ", 40, 3);
+    PrintText(" \\___ \\ / _ \\ '_ ` _ \\ / _ \\/ __| __/ _ \\ '__|", 40, 4);
+    PrintText("   ___)|  __/ | | | | |  __/\\__ \\ ||  __/ |   ", 40, 5);
+    PrintText(" |____/ \\___|_| |_| |_|\\___||___/\\__\\___|_|   ", 40, 6);
+
+    TextBgColor(0, 3);
+    DrawRectangle(25, 8, 80, 12, 3);
+    PrintText("Enter semester (1,2 or 3):", 35, 10);
+    PrintText("Enter school year of the semester (Ex:2023-2024,...):", 35, 12);
+    PrintText("Enter start date (dd/mm/yyyy):", 35, 14);
+    PrintText("Enter end date (dd/mm/yyyy):", 35, 16);
+    DrawRectangle(35, 11, 35, 1, 15);
+    DrawRectangle(35, 13, 35, 1, 15);
+    DrawRectangle(35, 15, 35, 1, 15);
+    DrawRectangle(35, 17, 35, 1, 15);
+
+    GotoXY(35, 11);
+    if (InsertNameSemester(semester.NameSemester) == 0) return;
+
+    do {
+        GotoXY(35, 13);
+        if (InsertSchoolYear(semester.SchoolYear) == 0) return;
+        int check = CheckSchoolYear(semester);
+        if (check != 1) {
+            HidePointer();
+            TextBgColor(4, 3);
+            GotoXY(35, 19);
+            if (check == 0)
+                PrintText("ERROR:SCHOOL YEAR MUST HAVE FORMAT LIKE YYYY-YYYY,PRESS ENTER TO TRY AGAIN", 30, 19);
+            else if (check == -1)
+                PrintText("ERROR:SCHOOL YEAR WASN'T EXISTED, PRESS ENTER TO TRY AGAIN", 30, 19);
+            else if (check == -2)
+                PrintText("ERROR:SEMESTER WAS EXISTED IN SCHOOL YEAR, PRESS ENTER TO TRY AGAIN", 30, 19);
+            else if (check == -3)
+                PrintText("ERROR:SEMESTER ISN'T COME UP,PRESS ENTER TO TRY AGAIN", 30, 19);
+            ch = getch();
+            DrawRectangle(30, 19, 75, 1, 3);
+            DrawRectangle(35, 13, 10, 1, 15);
+            TextBgColor(0, 15);
+            semester.SchoolYear = "";
+            ShowPointer();
+        }
+        else break;
+    } while (true);
+
+    do {
+        GotoXY(35, 15);
+        if (InsertDate(semester.DateBegin) == 0) return;
+        int check = CheckDate(semester.DateBegin);
+        if (check == 0) {
+            TextBgColor(4, 3);
+            GotoXY(35, 18);
+            HidePointer();
+            cout << "ERROR: DATE MUST HAVE FORMAT LIKE DD/MM/YYYY (01/02/2020)";
+            PrintText("PRESS ENTER TO TRY AGAIN", 35, 19);
+            ch = getch();
+            DrawRectangle(30, 18, 70, 1, 3);
+            DrawRectangle(30, 19, 70, 1, 3);
+            DrawRectangle(35, 15, 10, 1, 15);
+            TextBgColor(0, 15);
+            semester.DateBegin.clear();
+            ShowPointer();
+        }
+        else break;
+    } while (true);
+
+    do {
+        GotoXY(35, 17);
+        if (InsertDate(semester.DateEnd) == 0) return;
+        int check = CheckDate(semester.DateEnd);
+        if (check == 0) {
+            TextBgColor(4, 3);
+            GotoXY(35, 18);
+            HidePointer();
+            cout << "ERROR: DATE MUST HAVE FORMAT LIKE DD/MM/YYYY (01/02/2020)";
+            PrintText("PRESS ENTER TO TRY AGAIN", 35, 19);
+            ch = getch();
+            DrawRectangle(30, 18, 70, 1, 3);
+            DrawRectangle(30, 19, 70, 1, 3);
+            DrawRectangle(35, 17, 10, 1, 15);
+            TextBgColor(0, 15);
+            semester.DateEnd.clear();
+            ShowPointer();
+        }
+        else break;
+    } while (true);
+
+    SemesterNow = semester.NameSemester;
+    CreateFolder("Data//SchoolYear//" + semester.SchoolYear + "//Semester" + semester.NameSemester);
+    CreateFolder("Data//SchoolYear//" + semester.SchoolYear + "//Semester" + semester.NameSemester + "//Class");
+    CreateFolder("Data//SchoolYear//" + semester.SchoolYear + "//Semester" + semester.NameSemester + "//Course");
+    CreateFolder("Data//SchoolYear//" + semester.SchoolYear + "//Semester" + semester.NameSemester + "//Course//score");
+    UpDateFileCSV(semester);
+    UpDateFileInfo(semester);
+
+    fstream file;
+    file.open("Data//SchoolYear//" + semester.SchoolYear + "//Semester" + semester.NameSemester + "//course_info.csv", ios::out);
+    file << "ID,COURSE NAME,TEACHER NAME,NUMBER OF CREDITS,MAX STUDENT,DAY,SESSION1,SESSION2";
+    file.close();
+
+    fstream fileCourse;
+    fileCourse.open("Data//SchoolYear//" + semester.SchoolYear + "//Semester" + semester.NameSemester + "//RegistrationCourseSession.txt", ios::out);
+    fileCourse << 0;
+    fileCourse.close();
+
+    fstream class_info;
+    class_info.open("Data//SchoolYear//" + semester.SchoolYear + "/class_info.csv", ios::in);
+    if (!class_info.is_open()) return;
+    string line;
+    getline(class_info, line);
+    while (!class_info.eof()) {
+        getline(class_info, line);
+        string className = line.substr(line.find(",") + 1, line.find(",", line.find(",") + 1) - line.find(",") - 1);
+        fstream fileClass, fileStudent;
+        fileClass.open("Data/SchoolYear/" + semester.SchoolYear + "/Semester" + semester.NameSemester + "/Class/" + className + ".csv", ios::out);
+        fileStudent.open("Data/SchoolYear/" + semester.SchoolYear + "/" + className + ".csv", ios::in);
+        string studentID;
+        getline(fileStudent, studentID);
+        int t = 1;
+        while (!fileStudent.eof()) {
+            getline(fileStudent, studentID);
+            studentID = studentID.substr(studentID.find(",") + 1, studentID.find(",", studentID.find(",") + 1) - studentID.find(",") - 1);
+            if (t == 1) {
+                fileClass << studentID; t++;
+            }
+            else fileClass << endl << studentID;
+        }
+    }
+
+    HidePointer();
+    TextBgColor(2, 3);
+    PrintText("CREATE SEMESTER SUCCESSFUL,PRESS ENTER BACK TO MENU !!!", 35, 19);
+    ch = getch();
+    TextBgColor(0, 15);
+}
+
+void CreateRegistrationCourse() {
+    system("cls");
+    string DateStart, DateEnd;
+    char ch;
+    TextBgColor(4, 7);
+    PrintText("  ____ ____  _____    _  _____ _____    ____ ___  _   _ ____  ____  _____ ", 25, 2);
+    PrintText(" / ___|  _ \\| ____|  / \\|_   _| ____|  / ___/ _ \\| | | |  _ \\/ ___|| ____|", 25, 3);
+    PrintText("| |   | |_) |  _|   / _ \\ | | |  _|   | |  | | | | | | | |_) \\___ \\|  _|  ", 25, 4);
+    PrintText("| |___|  _< | |___ / ___ \\| | | |___  | |__| |_| | |_| |  _ < ___) | |___ ", 25, 5);
+    PrintText(" \\____|_| \\_\\_____/_/   \\_\\_| |_____|  \\____\\___/ \\___/|_| \\_\\____/|_____|", 25, 6);
+
+    PrintText(" ____  _____ ____ ___ ____ _____ ____      _  _____ ___ ___  _   _ ", 28, 8);
+    PrintText("|  _ \\| ____/ ___|_ _/ ___|_   _|  _ \\    / \\|_   _|_ _/ _ \\| \\ | |", 28, 9);
+    PrintText("| |_) |  _|| |  _ | |\\___ \\ | | | |_) |  / _ \\ | |  | | | | |  \\| |", 28, 10);
+    PrintText("|  _ <| |__| |_| || | ___) || | |  _ <  / ___ \\| |  | | |_| | |\\  |", 28, 11);
+    PrintText("|_| \\_\\_____\\____|___|____/ |_| |_| \\_\\/_/   \\_\\_| |___\\___/|_| \\_|", 28, 12);
+
+    PrintText(" ____  _____ ____ ____ ___ ___  _   _ ", 35, 14);
+    PrintText("/ ___|| ____/ ___/ ___|_ _/ _ \\| \\ | |", 35, 15);
+    PrintText("\\___ \\|  _| \\___ \\___ \\| | | | |  \\| |", 35, 16);
+    PrintText(" ___) | |___ ___) |__) | | |_| | |\\  |", 35, 17);
+    PrintText("|____/|_____|____/____/___\\___/|_| \\_|", 35, 18);
+
+    string year, semester;
+    DetermineYearSemesterNow(year, semester);
+    if (stoi(semester.substr(8, 1), 0, 10) == 0) {
+        TextBgColor(4, 15);
+        PrintText("YOU HAVEN'T CREATED SEMESTER YET, PRESS ENTER TO BACK TO MENU.", 40, 10);
+        ch = getch();
+    }
+    else {
+        DrawRectangle(30, 20, 50, 7, 3);
+        TextBgColor(0, 3);
+        PrintText("START DATE (DD/MM/YYYY):", 35, 21);
+        PrintText("END DATE (DD/MM/YYYY):", 35, 24);
+        DrawRectangle(35, 22, 40, 1, 15);
+        DrawRectangle(35, 25, 40, 1, 15);
+
+        string FileName = "Data//SchoolYear//" + year + "//" + semester + "//RegistrationCourseSession.txt";
+        do {
+            GotoXY(35, 22);
+            if (InsertDate(DateStart) == 0) return;
+            int check = CheckDate(DateStart);
+            if (check == 0) {
+                TextBgColor(4, 15);
+                GotoXY(20, 28);
+                HidePointer();
+                cout << "ERROR: DATE MUST HAVE FORMAT LIKE DD/MM/YYYY (01/02/2020,..), PRESS ENTER TO TRY AGAIN";
+                ch = getch();
+                DrawRectangle(20, 28, 90, 1, 15);
+                DrawRectangle(35, 22, 40, 1, 15);
+                TextBgColor(0, 15);
+                DateStart.clear();
+                ShowPointer();
+            }
+            else break;
+        } while (true);
+
+        do {
+            GotoXY(35, 25);
+            if (InsertDate(DateEnd) == 0) return;
+            int check = CheckDate(DateEnd);
+            if (check == 0) {
+                TextBgColor(4, 15);
+                GotoXY(20, 28);
+                HidePointer();
+                cout << "ERROR: DATE MUST HAVE FORMAT LIKE DD/MM/YYYY (01/02/2020,..), PRESS ENTER TO TRY AGAIN";
+                ch = getch();
+                DrawRectangle(20, 28, 90, 1, 15);
+                DrawRectangle(35, 25, 40, 1, 15);
+                TextBgColor(0, 15);
+                DateEnd.clear();
+                ShowPointer();
+            }
+            else break;
+        } while (true);
+        HidePointer();
+        fstream file;
+        file.open(FileName, ios::out);
+        file << 1;
+        file << endl << "Date start: " << DateStart;
+        file << endl << "Date end:" << DateEnd;
+        file.close();
+        TextBgColor(9, 15);
+        PrintText("CREATE COURSE REGISTRATION SESSION SUCCESSFUL !!! ,PRESS ENTER TO BACK TO MENU", 20, 28);
+        ch = getch();
+    }
+    TextBgColor(0, 15);
+}
